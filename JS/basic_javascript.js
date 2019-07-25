5d6f1852f53cdf7d57bb91e57f915d035002fe6c
Josh <joshdnorris@gmail.com>
2019-07-25 15:06:30 -0500

    Basic JavaScript practice

---
+++ JS/basic_javascript.js
@@ -0,0 +1,7 @@
+document.write('Hello world');
+
+function first_function_yay() {
+    var String = "ow not that hard";
+    var result = String.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML = result;
+}
