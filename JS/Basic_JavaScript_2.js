8e86785c120e54b7fa7b30471743b31f28fb2955
Josh <joshdnorris@gmail.com>
2019-07-25 15:38:43 -0500

    Basic JavaScript 2 projects

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function mouseOver () {
+    var x = "The color changed!";
+    var y = x.fontcolor("red");
+    document.getElementById("over").innerHTML = y;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this class";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
--- JS/basic_javascript.js
+++ JS/basic_javascript.js
@@ -5,3 +5,8 @@ function first_function_yay() {
     var result = String.fontcolor("red");
     document.getElementById("Red_Text").innerHTML = result;
 }
+
+function mouse_over_function() {
+    var string = "you did it!";
+    document.getElementById("mouse_over").innerHTML = string;
+}
