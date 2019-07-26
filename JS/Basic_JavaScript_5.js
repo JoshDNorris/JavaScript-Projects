aae5ebf983fefa30db94cfc7f3c59b69da670376
Josh <joshdnorris@gmail.com>
2019-07-26 09:15:50 -0500

    Basic HTML and JS 5

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,20 @@
+function getType () {
+    document.getElementById("getType").innerHTML = isNaN('758');
+}
+document.writeln(2E310);
+document.writeln(10>2);
+document.writeln(50>100);
+console.log(2+2);
+document.writeln("fifty" + 5);
+console.log("Is five greater than 100?");
+console.log(5>100);
+console.log("!");
+document.writeln(5==6);
+document.writeln(5===5);
+document.writeln(5==='five');
+document.writeln(5==='5');
+document.writeln(5===4);
+document.writeln(5<2 && 2<3);
+document.writeln(5<10 && 10<20);
+document.writeln(5<10 || 10<5);
+document.writeln(!(5<10));
